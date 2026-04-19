f18e4555e1956dd068923a250cd2d2058237c5fc
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2026-04-19 20:48:25 -0300

    feat(livraria): create autor class

---
+++ model/Autor.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Autor {
+    private final String id;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Autor(String nome, LocalDate dataNascimento) {
+        this.id = UUID.randomUUID().toString();
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+}
