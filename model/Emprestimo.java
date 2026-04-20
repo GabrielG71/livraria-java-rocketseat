b2ca495b220c5ee6ffd394704f4fc6b4a5ff50e2
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2026-04-19 21:11:50 -0300

    feat(livraria)): create emprestimo class

---
+++ model/Emprestimo.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Emprestimo {
+    private final String id;
+    private Livro livro;
+    private String nomeCliente;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
+
+    public Emprestimo(Livro livro, String nomeCliente, LocalDate dataEmprestimo, LocalDate dataDevolucao) {
+        this.id = UUID.randomUUID().toString();
+        this.livro = livro;
+        this.nomeCliente = nomeCliente;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+}
